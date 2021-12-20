87075170ed92749865cabe04c20396bd3f1e0999
william may <wmay@hotmail.es>
2021-12-20 15:19:58 -0500

    comienzo1.2

---
+++ app/src/main/java/com/wmay/tuciudad/RegistroCiudadActivity.kt
@@ -0,0 +1,25 @@
+package com.wmay.tuciudad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class RegistroCiudadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_ciudad)
+
+        val registrarButton: Button = findViewById(R.id.registrar_button)
+        val nombreEditText: EditText = findViewById(R.id.nombre_editTextTextPersonName)
+        val infoTextView: TextView = findViewById(R.id.info_text_View)
+        val
+                registrarButton.setOnClickListener {
+            val nombre = nombreEditText.text
+            infoTextView.text = nombre
+
+        }
+
+    }
+}
