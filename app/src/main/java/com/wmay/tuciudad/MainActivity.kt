8b206195a519884e27a1b2ea7dd37ef6e96a6250
william may <wmay@hotmail.es>
2021-12-20 03:00:42 -0500

    comienzo

--- app/src/main/java/com/wmay/tuciudad/MainActivity.kt
+++ app/src/main/java/com/wmay/tuciudad/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val tituloTextView : TextView =
+
     }
+
 }
