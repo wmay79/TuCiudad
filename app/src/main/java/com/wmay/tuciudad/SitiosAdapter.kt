9a0522fff8635c21785f3df60ada309ade790ea0
william may <wmay@hotmail.es>
2021-12-21 06:20:34 -0500

    comienzo2.1adapter, recycler2.1

--- app/src/main/java/com/wmay/tuciudad/SitiosAdapter.kt
+++ app/src/main/java/com/wmay/tuciudad/SitiosAdapter.kt
@@ -14,7 +14,7 @@ class SitiosAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.activity_list_sitios2, parent, false)
+            .inflate(R.layout.card_view_sitio_item, parent, false)
         return ViewHolder(view)
     }
 
